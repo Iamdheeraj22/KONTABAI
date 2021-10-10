3acf3f5d726a70b2b37d98dbf10f44c9719080ac
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2021-10-11 01:05:36 +0530

    Set the UI and Functionality of tablayout in DriverDashBoard.

--- app/src/main/java/com/example/kontabai/Activities/DriverDashBoard.java
+++ app/src/main/java/com/example/kontabai/Activities/DriverDashBoard.java
@@ -1,16 +1,33 @@
 package com.example.kontabai.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.kontabai.Activities.Fragments.AcceptedRequestFragment;
+import com.example.kontabai.Activities.Fragments.PendingRequestFragment;
+import com.example.kontabai.Classes.ViewPageAdapter;
 import com.example.kontabai.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class DriverDashBoard extends AppCompatActivity {
-
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_dash_board);
+        //initViews();
+        tabLayout=findViewById(R.id.tab_layout);
+        viewPager=findViewById(R.id.view_pager);
+        setUpTablayout();
+    }
+    private void setUpTablayout(){
+        ViewPageAdapter viewPagerAdapter= new ViewPageAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new AcceptedRequestFragment(),"Accepted");
+        viewPagerAdapter.addFragment(new PendingRequestFragment(),"Pending");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
