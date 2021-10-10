0c54e90c605146fc7da938ede07e8153735b5ce8
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2021-10-11 01:04:22 +0530

    Add Two fragments for AcceptedRide and PendingRide

---
+++ app/src/main/java/com/example/kontabai/Activities/Fragments/AcceptedRequestFragment.java
@@ -0,0 +1,19 @@
+package com.example.kontabai.Activities.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kontabai.R;
+public class AcceptedRequestFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_accepted_request, container, false);
+    }
+}
