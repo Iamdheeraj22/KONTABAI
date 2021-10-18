88ee70e8c85a9cb9da3ab4cd4354b1bc4e1263b4
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-10-18 10:34:10 +0530

    Verification process done

--- app/src/main/java/com/example/kontabai/Activities/VerificationActivity.java
+++ app/src/main/java/com/example/kontabai/Activities/VerificationActivity.java
@@ -12,17 +12,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kontabai.Activities.UserSide.UserSideProfileCreation;
 import com.example.kontabai.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class VerificationActivity extends AppCompatActivity {
     TextView verifyButton;
     ImageView backButton;
     EditText verificationCode;
     FirebaseAuth firebaseAuth;
-//    AlertDialog alertDialog;
     String mobilenumber,verificationId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +38,7 @@ public class VerificationActivity extends AppCompatActivity {
             }else if(verification.length()!=6){
                 verificationCode.setError("Enter valid code!");
             }else {
-                setUpTheAlertBox();
+                verifyCode(verification);
                 verifyButton.setBackgroundResource(R.drawable.screen_background);
             }
         });
@@ -44,56 +46,39 @@ public class VerificationActivity extends AppCompatActivity {
                 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)));
     }
 
-//    private void verifyCode(String code) {
-//        PhoneAuthCredential authCredential= PhoneAuthProvider.getCredential(verificationId,code);
-//        signWithPhoneCredentail(authCredential);
-//    }
-//
-//    private void signWithPhoneCredentail(PhoneAuthCredential authCredential)
-//    {
-//        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(task -> {
-//            if(task.isSuccessful()){
-//                alertDialog=new AlertDialog.Builder(VerificationActivity.this,R.style.verification_done).create();
-//                View view=getLayoutInflater().inflate(R.layout.confirmation_dialog,null,false);
-//                alertDialog.setView(view);
-//                alertDialog.setCancelable(false);
-//                alertDialog.show();
-//                TextView okButton=view.findViewById(R.id.okButton);
-//                okButton.setOnClickListener(view1 -> {
-//                    Intent intent=new Intent(VerificationActivity.this,UserSideProfileCreation.class);
-//                    intent.putExtra("number",mobilenumber);
-//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-//                    startActivity(intent);
-//                    alertDialog.dismiss();
-//                    finish();
-//                });
-//            }else{
-//                alertDialog.dismiss();
-//                Toast.makeText(VerificationActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
-//    }
-
-
-    private void setUpTheAlertBox(){
-        AlertDialog alertDialog=new AlertDialog.Builder(VerificationActivity.this,R.style.verification_done).create();
-        View view= LayoutInflater.from(VerificationActivity.this).inflate(R.layout.confirmation_dialog,null,false);
-        alertDialog.setView(view);
-        alertDialog.show();
-        alertDialog.setCancelable(false);
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                alertDialog.dismiss();
-                startActivity(new Intent(VerificationActivity.this, UserSideProfileCreation.class)
-                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
-            }
-        },3000);
-
+    private void verifyCode(String code) {
+        PhoneAuthCredential authCredential= PhoneAuthProvider.getCredential(verificationId,code);
+        signWithPhoneCredentail(authCredential);
     }
 
+    private void signWithPhoneCredentail(PhoneAuthCredential authCredential)
+    {
+        AlertDialog alertDialog;
+        alertDialog=new AlertDialog.Builder(VerificationActivity.this,R.style.verification_done).create();
+        View view=getLayoutInflater().inflate(R.layout.progress_dialog,null,false);
+        alertDialog.setView(view);
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                alertDialog.dismiss();
+                AlertDialog verificationDone= new AlertDialog.Builder(VerificationActivity.this).create();
+                View view1=LayoutInflater.from(VerificationActivity.this).inflate(R.layout.confirmation_dialog,null,false);
+                verificationDone.setView(view1);
+                verificationDone.show();
+                Handler handler=new Handler();
+                handler.postDelayed(() -> {
+                    verificationDone.dismiss();
+                    startActivity(new Intent(VerificationActivity.this,UserSideProfileCreation.class)
+                         .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+                },3000);
+            }else{
+                alertDialog.dismiss();
+                Toast.makeText(VerificationActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
     private void initViews()
     {
         verifyButton=findViewById(R.id.continueButton);
