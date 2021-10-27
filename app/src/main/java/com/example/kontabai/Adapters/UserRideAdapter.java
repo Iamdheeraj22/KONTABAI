eff4f6145d3976c02757ef7b89364d277643e0c4
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-10-27 18:44:26 +0530

    Set the views according firebasedata

--- app/src/main/java/com/example/kontabai/Adapters/UserRideAdapter.java
+++ app/src/main/java/com/example/kontabai/Adapters/UserRideAdapter.java
@@ -13,68 +13,54 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.kontabai.Classes.UserSideRideModel;
 import com.example.kontabai.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 import java.util.ArrayList;
 
-public class UserRideAdapter extends RecyclerView.Adapter<UserRideAdapter.UserRideViewHolder>
+public class UserRideAdapter extends FirebaseRecyclerAdapter<UserSideRideModel,UserRideAdapter.UserRideViewHolder>
 {
-    Context mContext;
-    ArrayList<UserSideRideModel> userSideRideModels;
 
-    public UserRideAdapter(Context mContext, ArrayList<UserSideRideModel> userSideRideModels) {
-        this.mContext = mContext;
-        this.userSideRideModels = userSideRideModels;
+    public UserRideAdapter(@NonNull FirebaseRecyclerOptions<UserSideRideModel> options) {
+        super(options);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onBindViewHolder(@NonNull UserRideViewHolder holder, int position, @NonNull UserSideRideModel model) {
+        String status=model.getStatus();
+        if(status.equalsIgnoreCase("pending")){
+            holder.requestStatus.setText(status);
+            holder.requestStatus.setTextColor(Color.RED);
+        }else if(status.equalsIgnoreCase("accepted")){
+            holder.requestStatus.setText(status);
+            holder.requestStatus.setTextColor(Color.GREEN);
+        }else if(status.equalsIgnoreCase("rejected")){
+            holder.requestStatus.setText(status);
+            holder.requestStatus.setTextColor(Color.RED);
+        }
+        holder.requestLocation.setText("Pickup Location: "+model.getPickup_address());
+        holder.requestOrder.setText("RIDE#"+model.getCount());
+        holder.requestTime.setText(model.getDate());
+        holder.requestStatus.setText(model.getStatus());
     }
 
     @NonNull
     @Override
     public UserRideViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(mContext).inflate(R.layout.user_ride,null,false);
+        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.user_ride,null,false);
         return new UserRideViewHolder(view);
-    }
-
-    @SuppressLint("SetTextI18n")
-    @Override
-    public void onBindViewHolder(@NonNull UserRideViewHolder holder, int position) {
-        UserSideRideModel userSideRideModel = userSideRideModels.get(position);
-        String pickLocation= userSideRideModel.getLocation();
-        String date= userSideRideModel.getDate();
-        String id= userSideRideModel.getId();
-        String status= userSideRideModel.getStatus();
-
-        switch (status) {
-            case "Pending":
-                holder.status.setText("Pending");
-                holder.status.setTextColor(Color.RED);
-                break;
-            case "Rejected":
-                holder.status.setText("Rejected");
-                holder.status.setTextColor(Color.RED);
-                break;
-            case "Accepted":
-                holder.status.setText("Accepted");
-                holder.status.setTextColor(Color.GREEN);
-                break;
-        }
-        holder.pickupLocation.setText("Pickup Location: "+pickLocation);
-        holder.id.setText("Ride#"+id);
-        holder.date.setText(date);
-    }
-
-    @Override
-    public int getItemCount() {
-        return userSideRideModels.size();
     }
 
     static class UserRideViewHolder extends RecyclerView.ViewHolder
     {
-        TextView pickupLocation,status,id,date;
+        TextView requestOrder,requestTime,requestLocation,requestStatus;
         public UserRideViewHolder(@NonNull View itemView) {
             super(itemView);
-            pickupLocation=itemView.findViewById(R.id.rideLocation);
-            status=itemView.findViewById(R.id.rideStatus);
-            id=itemView.findViewById(R.id.rideId);
-            date=itemView.findViewById(R.id.rideDateTime);
+            requestLocation=itemView.findViewById(R.id.rideLocation);
+            requestOrder=itemView.findViewById(R.id.rideId);
+            requestTime=itemView.findViewById(R.id.rideDateTime);
+            requestStatus=itemView.findViewById(R.id.rideStatus);
         }
     }
 }
