cad4ed98936c8fc42e0e98abd1ad162c9628c43d
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-10-27 18:43:03 +0530

    Set the all views according components

--- app/src/main/java/com/example/kontabai/Adapters/DriverSideAcceptRideAdapter.java
+++ app/src/main/java/com/example/kontabai/Adapters/DriverSideAcceptRideAdapter.java
@@ -1,6 +1,7 @@
 package com.example.kontabai.Adapters;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,48 +13,58 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.kontabai.Classes.DriverSideRideModel;
 import com.example.kontabai.R;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class DriverSideAcceptRideAdapter extends FirebaseRecyclerAdapter<DriverSideRideModel, DriverSideAcceptRideAdapter.DriverViewModel>
-{
-    public DriverSideAcceptRideAdapter(@NonNull FirebaseRecyclerOptions<DriverSideRideModel> options) {
-        super(options);
+import java.util.ArrayList;
+
+public class DriverSideAcceptRideAdapter extends RecyclerView.Adapter<DriverSideAcceptRideAdapter.DriverViewModel> {
+    ArrayList<DriverSideRideModel> arrayList;
+    Context context;
+
+    public DriverSideAcceptRideAdapter(ArrayList<DriverSideRideModel> arrayList, Context context) {
+        this.context = context;
+        this.arrayList=arrayList;
     }
+
     @SuppressLint("SetTextI18n")
     @Override
-    protected void onBindViewHolder(@NonNull DriverViewModel holder, int position, @NonNull DriverSideRideModel model) {
-        if(model.getDriverId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
-            String status= model.getStatus();
-            if(status.equalsIgnoreCase("accepted")){
-                holder.requestStatus.setText("Rejected");
-                holder.requestStatus.setTextColor(Color.RED);
-            }else if (status.equalsIgnoreCase("rejected")){
-                holder.requestStatus.setText("Accepted");
-                holder.requestStatus.setTextColor(Color.GREEN);
-            }
-            holder.requestLocation.setText("Pickup Location: "+model.getPickup_address());
-            holder.requestOrder.setText("RIDE#"+model.getRequest_order());
-            holder.requestTime.setText(model.getDate());
-            holder.requestStatus.setText(model.getStatus());
+    public void onBindViewHolder(@NonNull DriverViewModel holder, int position) {
+        DriverSideRideModel model = arrayList.get(position);
+        if (model.getStatus().equalsIgnoreCase("completed")) {
+            holder.requestStatus.setText("completed");
+            holder.requestStatus.setTextColor(Color.GREEN);
+        } else if (model.getStatus().equalsIgnoreCase("accepted")) {
+            holder.requestStatus.setText("Accepted");
+            holder.requestStatus.setTextColor(Color.GREEN);
+        } else if (model.getStatus().equalsIgnoreCase("rejected")) {
+            holder.requestStatus.setText("Rejected");
+            holder.requestStatus.setTextColor(Color.RED);
         }
+        holder.requestLocation.setText("Pickup Location: " + model.getPickup_address());
+        holder.requestOrder.setText("RIDE#" + model.getRequest_order());
+        holder.requestTime.setText(model.getDate());
     }
+
     @NonNull
     @Override
     public DriverViewModel onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        @SuppressLint("InflateParams") View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.user_ride,null,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_ride, null, false);
         return new DriverViewModel(view);
     }
-    static class DriverViewModel extends RecyclerView.ViewHolder
-    {
-        TextView requestOrder,requestTime,requestLocation,requestStatus;
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    static class DriverViewModel extends RecyclerView.ViewHolder {
+        TextView requestOrder, requestTime, requestLocation, requestStatus;
+
         public DriverViewModel(@NonNull View itemView) {
             super(itemView);
-            requestLocation=itemView.findViewById(R.id.rideLocation);
-            requestOrder=itemView.findViewById(R.id.rideId);
-            requestTime=itemView.findViewById(R.id.rideDateTime);
-            requestStatus=itemView.findViewById(R.id.rideStatus);
+            requestLocation = itemView.findViewById(R.id.rideLocation);
+            requestOrder = itemView.findViewById(R.id.rideId);
+            requestTime = itemView.findViewById(R.id.rideDateTime);
+            requestStatus = itemView.findViewById(R.id.rideStatus);
         }
     }
 }
