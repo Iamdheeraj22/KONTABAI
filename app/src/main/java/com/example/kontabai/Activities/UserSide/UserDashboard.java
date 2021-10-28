74424d314d6655d33677169eaa361347a48686d5
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-10-28 18:50:05 +0530

    Show the ride according reverse

--- app/src/main/java/com/example/kontabai/Activities/UserSide/UserDashboard.java
+++ app/src/main/java/com/example/kontabai/Activities/UserSide/UserDashboard.java
@@ -1,5 +1,6 @@
 package com.example.kontabai.Activities.UserSide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,21 +9,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.example.kontabai.Adapters.DriverSidePendingRideAdapter;
 import com.example.kontabai.Adapters.UserRideAdapter;
-import com.example.kontabai.Classes.DriverSideRideModel;
+import com.example.kontabai.Classes.RideModel;
 import com.example.kontabai.Classes.UserSideRideModel;
 import com.example.kontabai.R;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class UserDashboard extends AppCompatActivity {
     RecyclerView recyclerView;
     UserRideAdapter userRideAdapter;
+    ArrayList<RideModel> arrayList;
     String currentUser;
     ImageView backButton;
     @Override
@@ -30,7 +38,6 @@ public class UserDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
         initViews();
-        setTheValuesInRecyclerView();
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,15 +48,26 @@ public class UserDashboard extends AppCompatActivity {
     }
     private void setTheValuesInRecyclerView()
     {
-        FirebaseRecyclerOptions<UserSideRideModel> options=
-                new FirebaseRecyclerOptions.Builder<UserSideRideModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("userRequest").child(currentUser),UserSideRideModel.class)
-                        .build();
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this);
-        userRideAdapter=new UserRideAdapter(options);
-        recyclerView.setLayoutManager(layoutManager);
-        userRideAdapter.startListening();
-        recyclerView.setAdapter(userRideAdapter);
+        DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference().child("userRequest").child(currentUser);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                arrayList=new ArrayList<>();
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    RideModel rideModel =dataSnapshot.getValue(RideModel.class);
+                    arrayList.add(rideModel);
+                }
+                Collections.reverse(arrayList);
+                userRideAdapter=new UserRideAdapter(UserDashboard.this,arrayList);
+                RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(UserDashboard.this);
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(userRideAdapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(UserDashboard.this, "Faild :-"+error, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initViews() {
@@ -57,6 +75,13 @@ public class UserDashboard extends AppCompatActivity {
         backButton=findViewById(R.id.userBackButton);
         currentUser=FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setTheValuesInRecyclerView();
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
