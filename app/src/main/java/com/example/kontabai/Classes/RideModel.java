6fef9589ac61a8b49b4c98f1edafe9e86165e6e7
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-10-28 18:49:08 +0530

    Change the components name

--- app/src/main/java/com/example/kontabai/Classes/RideModel.java
+++ app/src/main/java/com/example/kontabai/Classes/RideModel.java
@@ -1,31 +1,28 @@
 package com.example.kontabai.Classes;
 
-public class DriverSideRideModel
+public class RideModel
 {
     private String pickup_address;
     private String status;
     private String date;
-    private String driverId;
+    private String driver_id;
     private String id;
-    private String userId;
+    private String user_id;
     private String request_order;
 
-    public DriverSideRideModel(String pickup_address, String status, String date, String request_order,String driverId,String id,String userId) {
+    public RideModel(String pickup_address, String status, String date, String request_order, String driver_id, String id, String user_id) {
         this.pickup_address = pickup_address;
         this.status = status;
-        this.userId=userId;
+        this.user_id=user_id;
         this.date=date;
         this.request_order = request_order;
-        this.driverId=driverId;
+        this.driver_id=driver_id;
         this.id=id;
     }
-    public DriverSideRideModel() {
+    public RideModel() {
     }
 
 
-    public String getUserId() {
-        return userId;
-    }
 
     public String getId() {
         return id;
@@ -42,7 +39,12 @@ public class DriverSideRideModel
     public String getPickup_address() {
         return pickup_address;
     }
-    public String getDriverId() {
-        return driverId;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public String getDriver_id() {
+        return driver_id;
     }
 }
