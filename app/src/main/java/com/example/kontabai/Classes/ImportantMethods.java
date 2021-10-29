5756f5d7bc5971997a2a17337feaeab0d8c6d45a
Dheeraj Prajapat <dheeraj.quadtrics@gmail.com>
2021-10-29 18:09:56 +0530

    Add one method for get uri of photo from camera

--- app/src/main/java/com/example/kontabai/Classes/ImportantMethods.java
+++ app/src/main/java/com/example/kontabai/Classes/ImportantMethods.java
@@ -2,8 +2,13 @@ package com.example.kontabai.Classes;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.webkit.MimeTypeMap;
+
+import java.io.ByteArrayOutputStream;
 
 public class ImportantMethods
 {
@@ -12,4 +17,26 @@ public class ImportantMethods
         MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
         return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
     }
+
+    public static Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+//    public String getRealPathFromURI(Uri uri) {
+//        String path = "";
+//        if (getContentResolver() != null) {
+//            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+//            if (cursor != null) {
+//                cursor.moveToFirst();
+//                int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+//                path = cursor.getString(idx);
+//                cursor.close();
+//            }
+//        }
+//        return path;
+//    }
+
 }
